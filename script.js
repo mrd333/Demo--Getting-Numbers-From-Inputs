3a7f8c46f6d4d68bb7f9c1c7522a8b89717a249d
mrd333 <mrd333@users.noreply.github.com>
2019-03-20 08:30:02 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,9 @@
+$(".btn").click(function(){
+        var number = $(".ipt").val();
+        var newNum = parseInt(number) + 2;
+        $(".target").text(newNum);
+    });
+    
+    if("30" === "30"){
+        
+        } 
